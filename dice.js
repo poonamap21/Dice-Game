cf8b2729d408487e2ba14f587850b5fe0d30f9f6
poonamap21 <87647609+poonamap21@users.noreply.github.com>
2022-02-23 10:42:16 +0530

    Initial Commit

---
+++ dice.js
@@ -0,0 +1,16 @@
+
+var randomNumber1 = Math.floor(Math.random()*6 + 1);
+var image1 ="images/dice" + randomNumber1 +".png"
+document.querySelector(".img1").setAttribute("src",image1)
+var randomNumber2 = Math.floor(Math.random()*6 + 1);
+var image2 ="images/dice" + randomNumber2 +".png"
+document.querySelector(".img2").setAttribute("src",image2)
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "🚩 Play 1 Wins!";
+}
+else if (randomNumber2 > randomNumber1) {
+  document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
+}
+else {
+  document.querySelector("h1").innerHTML = "Draw!";
+}
